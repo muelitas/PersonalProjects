e0805e05aaa017c531754aabc6594764dfb1beb2
Mario <mario@pop-os.localdomain>
2020-06-13 10:48:44 -0600

    Added functions to ch2func.c

--- DataStructs/CH02/p02.c
+++ DataStructs/CH02/p02.c
@@ -16,58 +16,23 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
-//Function that prints an array of integers
-void printArr(int L, int arr[L]){
-	int i;
-	for(i=0; i<L; i++){
-		printf("%d\t", arr[i]);
-	}
-	printf("\n");
-	return;
-}
-
-//Sort in decreasing order
-void sort(int L, int A[L]){
-	int i, j, key;
-	for(j=L-2;j>=0;j--){
-		key = A[j];
-		i = j + 1;
-		while(i<L && A[i]>key){
-			A[i-1] = A[i];
-			i = i + 1;
-		}
-		A[i-1] = key;
-	}
-	return;
-}
-
-//Function that creates random integers and stores them in array
-void populateArr(int L, int A[L]){
-	srand(time(0)); //set different seed every time program runs
-	int i;
-	for(i=0; i<L; i++){
-		A[i] = rand()%1001;
-		//using "%1001" to limit numbers between 0 and 1000
-	}
-	return;
-}
+#include "ch2Funcs.h"
 
 int main(){
 	int length;
 	printf("How many values would you like your array to have? ");
 	scanf("%d", &length);
 	int A[length];
-	populateArr(length, A);
+	populateArray(length, A);
 
 	//Print Array
 	printf("Array A is:\n");
-	printArr(length, A);
+	printArray(length, A);
 
 	//Sort Array and Print Again
-	sort(length, A);
+	insert_sort_back(length, A);
 	printf("Sorted in descending order:\n");
-	printArr(length, A);
+	printArray(length, A);
 
 	return 0;
 }
