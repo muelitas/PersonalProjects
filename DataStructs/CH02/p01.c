9b154de6a1198b387ff20a26fc619b386dc2e985
Mario <mario@pop-os.localdomain>
2020-06-13 10:43:40 -0600

    Added functions to ch2func.c

--- DataStructs/CH02/p01.c
+++ DataStructs/CH02/p01.c
@@ -7,88 +7,41 @@
   * In this program I explore insertion sort. I have two sort
   * functions. One is the one I implemented on the get go and 
   * the second one is the one implmented by the book. I also
-  * have two "function-helpers"; one prints an integer array,
-  * the other one copies one array into another one.
+  * have three "function-helpers"; printArray, scanArray and
+  * copyArray.
   *
 ****************************************************************/ 
 
 #include <stdio.h>
-//Function that copies array X into Y
-void copyArr(int L, int X[L], int Y[L]){
-	int i;
-	for(i=0; i<L; i++){
-		Y[i] = X[i];
-	}
-	return;
-}
-
-//Function that prints an array of integers
-void printArr(int L, int arr[L]){
-	int i;
-	for(i=0; i<L; i++){
-		printf("%d\t", arr[i]);
-	}
-	printf("\n");
-	return;
-}
-
-//Insertion sort; method from the book
-void sort2(int L, int A[L]){
-	int i, j, key;
-	for(j=1;j<L;j++){
-		key = A[j];
-		i = j - 1;
-		while(i>=0 && A[i]>key){
-			A[i+1] = A[i];
-			i = i - 1;
-		}
-		A[i+1] = key;
-	}
-	return;
-}
-
-//Insertion sort; my method
-void sort1(int L, int arr[L]){
-	int i, j, k, val1, val2;
-	for(i=1;i<L;i++){
-		val1 = arr[i];
-		k=i;
-		for(j=i-1;j>=0;j--){
-			val2 = arr[j];
-			if(val1<val2){
-				arr[k] = val2;
-				arr[j] = val1;
-				k--;
-			}else{
-				break;
-			}
-		}
-	}
-}
-
+#include "ch2Funcs.h"
 int main(){
-	//Array given by the book in page 18
-	int A[] = {5,2,4,6,1,3};
-	int length = sizeof(A)/sizeof(A[0]);
+	//Array given by the book in page 18 is {5,2,4,6,1,3};
+	int L;
+	printf("How large is your array?\n");
+	scanf("%d", &L);
+	int A[L];
+	printf("Please input the values:\n");
+	scanArray(L, A);
+
 	//Create copy so it can be used in second sort function
-	int B[length];
-	copyArr(length,A,B);
+	int B[L];
+	copyArray(L,A,B);
 
 	//Print Arrays
 	printf("Array A is:\n");
-	printArr(length, A);
+	printArray(L, A);
 	printf("It's copy B is:\n");
-	printArr(length, B);
+	printArray(L, B);
 
 	//Sort A with my method
-	sort1(length, A);
+	insert_sort_mine(L, A);
 	printf("\nSorted array with my method:\n");
-	printArr(length, A);
+	printArray(L, A);
 	
 	//Sort B with book's method
-	sort2(length, B);
+	insert_sort_book(L, B);
 	printf("Sorted array with book's method:\n");
-	printArr(length, B);
+	printArray(L, B);
 
 	return 0;
 }
